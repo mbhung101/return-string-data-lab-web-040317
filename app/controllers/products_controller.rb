553a6ae82e3ac517e36a11f7fe181ae076a2e632
github username <github email address>
2017-05-23 15:48:55 -0400

    not downloading firefox

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,33 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def new
+    @object = Product.new()
+  end
+
+  def create
+    if Product.create(product_params)
+      redirect_to products_path
+    else
+      return "error"
+    end
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    render plain: (@product.inventory == 0 ? false : true)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:description)
+  end
 end
